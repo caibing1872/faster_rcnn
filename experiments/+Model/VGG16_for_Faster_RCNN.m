8934e564d8a1ae41a38a2fcee888485a779f09d8
hli2020 <hongyangli2020@gmail.com>
2016-07-26 21:57:11 +0800

    fixt some bugs after deploying the train process of stage 2

--- experiments/+Model/VGG16_for_Faster_RCNN.m
+++ experiments/+Model/VGG16_for_Faster_RCNN.m
@@ -3,6 +3,9 @@ function model = VGG16_for_Faster_RCNN(solver_name_rpn, test_file_rpn, ...
 % VGG 16layers (only finetuned from conv3_1)
 % for ilsvrc dataset!
 
+if nargin < 3
+  solver_name_fast_rcnn = ''; test_file_fast_rcnn = '';
+end
 model.mean_image = fullfile(pwd, 'models', 'pre_trained_models', 'vgg_16layers', 'mean_image');
 model.pre_trained_net_file = fullfile(pwd, 'models', 'pre_trained_models', 'vgg_16layers', 'vgg16.caffemodel');
 % Stride in input image pixels at the last conv layer
