0762028e1fb61e5360a3ddf4c428b9c2b9e6bd42
hli2020 <hongyangli2020@gmail.com>
2016-07-26 19:51:32 +0800

    change a few codes to run on ls149

--- functions/fast_rcnn/fast_rcnn_train.m
+++ functions/fast_rcnn/fast_rcnn_train.m
@@ -65,7 +65,7 @@ disp(opts);
 %% making tran/val data
 mkdir_if_missing('./output/training_test_data/');
 % training
-train_data_name = ['FCN_train_' conf.cache_base_proposal];
+train_data_name = ['FCN_train_' conf.data_name];
 if exist(sprintf('./output/training_test_data/%s.mat', train_data_name), 'file')
     
     ld = load(sprintf('./output/training_test_data/%s.mat', train_data_name));
@@ -85,7 +85,7 @@ else
 end
 
 % validation
-val_data_name = ['FCN_val_' conf.cache_base_proposal];
+val_data_name = ['FCN_val_' conf.data_name];
 if opts.do_val
     
     if exist(sprintf('./output/training_test_data/%s.mat', val_data_name), 'file')
@@ -113,7 +113,8 @@ end
 % try to train/val with images which have maximum size potentially,
 % to validate whether the gpu memory is enough
 num_classes = size(image_roidb_train(1).overlap, 2);
-if opts.binary, num_classes = 1; end
+fcn_get_minibatch = @fast_rcnn_get_minibatch;
+if opts.binary, num_classes = 1; fcn_get_minibatch = @fast_rcnn_get_minibatch_binary; end
 check_gpu_memory(conf, caffe_solver, num_classes, opts.do_val);
 
 %% training
@@ -131,7 +132,7 @@ while (iter_ < max_iter)
     % generate minibatch training data
     [shuffled_inds, sub_db_inds] = generate_random_minibatch(shuffled_inds, image_roidb_train, conf.ims_per_batch);
     [im_blob, rois_blob, labels_blob, bbox_targets_blob, bbox_loss_weights_blob] = ...
-        fast_rcnn_get_minibatch_binary(conf, image_roidb_train(sub_db_inds));
+        fcn_get_minibatch(conf, image_roidb_train(sub_db_inds));
     
     net_inputs = {im_blob, rois_blob, labels_blob, bbox_targets_blob, bbox_loss_weights_blob};
     caffe_solver.net.reshape_as_input(net_inputs);
