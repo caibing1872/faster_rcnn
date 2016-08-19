ba5c30bd7abf4ed50718ecaf520bd4f77e1aa711
hli2020 <hongyangli2020@gmail.com>
2016-08-19 10:57:23 +0800

    update

--- functions/fast_rcnn/fast_rcnn_test.m
+++ functions/fast_rcnn/fast_rcnn_test.m
@@ -260,12 +260,8 @@ else
             split_path = [fileparts(temp) '/split'];
             mkdir_if_missing(split_path);
             assert(length(imdb.image_ids) == length(aboxes));
-	    disp('debug');
-	    fprintf(temp);
-	    disp('');
-	    fprintf(fileparts(temp));
-	    keyboard;
-            for shit = 1:length(imdb.image_ids)
+            
+	    for shit = 1:length(imdb.image_ids)
                 boxes = aboxes{shit};
 		try
                    save([split_path '/' imdb.image_ids{shit} '.mat'], 'boxes');
